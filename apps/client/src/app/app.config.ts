49357d3c5ed5c72a3abbb04746c22b972b28700a
Sergey Bespalko <bespalkoser@gmail.com>
2024-01-06 13:22:09 +0200

    added animation on transition

--- apps/client/src/app/app.config.ts
+++ apps/client/src/app/app.config.ts
@@ -1,8 +1,11 @@
 import { ApplicationConfig, isDevMode } from '@angular/core'
-import { provideRouter, withEnabledBlockingInitialNavigation } from '@angular/router'
+import { provideRouter, withEnabledBlockingInitialNavigation, withViewTransitions } from '@angular/router'
 import { appRoutes } from './app.routes'
 import { provideServiceWorker } from '@angular/service-worker'
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes, withEnabledBlockingInitialNavigation()), provideServiceWorker('ngsw-worker.js', { enabled: !isDevMode() })]
+  providers: [
+    provideRouter(appRoutes, withEnabledBlockingInitialNavigation(), withViewTransitions()),
+    provideServiceWorker('ngsw-worker.js', { enabled: !isDevMode() })
+  ]
 }
